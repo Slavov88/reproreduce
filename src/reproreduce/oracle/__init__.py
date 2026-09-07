b725eadfa221397df9fd01311f564c35f6d546ad
Slavov88 <Slavov2009@outlook.com>
2026-09-07 19:01:51 +0300

    feat(oracle): add torch.compile numerical discrepancy detection

--- src/reproreduce/oracle/__init__.py
+++ src/reproreduce/oracle/__init__.py
@@ -1,4 +1,12 @@
 from .base import FailureOracle, OracleResult
 from .exception import ExceptionOracle, FailureFingerprint
+from .numerical import CompileDifferenceOracle, ExecutionOutcome
 
-__all__ = ["ExceptionOracle", "FailureFingerprint", "FailureOracle", "OracleResult"]
+__all__ = [
+    "CompileDifferenceOracle",
+    "ExceptionOracle",
+    "ExecutionOutcome",
+    "FailureFingerprint",
+    "FailureOracle",
+    "OracleResult",
+]
